fdfdc0a4dccbd8646a62cdfb483604717115efb2
Rifaazs <zaafir.mougammadou-zaccaria@supdevinci-edu.fr>
2025-01-15 15:59:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
 from health_utils import calculate_bmi, calculate_bmr
 
 app = Flask(__name__)
-CORS(app)
+
 
 @app.route('/health', methods=['GET'])
 def health():
